3fcc5a98cb759277aa91b01cec7010f90ca1f9a9
coderaiser <coderaiser@users.noreply.github.com>
2023-10-20 14:40:47 +0000

    chore: parse-git-index: actions: lint ☘️

--- example/example.js
+++ example/example.js
@@ -1,8 +1,1 @@
-const {readFileSync} = require('fs');
-const {join} = require('path');
-
-const parseGitIndex = require('..');
-
-const index = readFileSync(join(__dirname, '../.git/index'));
-
-console.log(parseGitIndex(index));
+'use strict';
